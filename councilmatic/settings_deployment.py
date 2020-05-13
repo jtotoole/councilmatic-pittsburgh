98c7731a0eee317272daab1e4e77055288708627
James O'Toole <jtotoole@gmail.com>
2020-05-13 19:24:04 -0400

    add solr_configs/hacky fix for council images

--- councilmatic/settings_deployment.py
+++ councilmatic/settings_deployment.py
@@ -50,7 +50,9 @@ ANALYTICS_TRACKING_CODE = ''
 # Google Maps API Key
 GOOGLE_API_KEY = os.getenv('GOOGLE_MAPS_KEY')
 
-HEADSHOT_PATH = os.path.join(os.path.dirname(__file__), '..'
-                             '/pittsburgh/static/images/')
+# HEADSHOT_PATH = os.path.join(os.path.dirname(__file__), '..'
+#                              '/pittsburgh/static/images/')
+
+STATIC_PATH = os.path.join(os.path.dirname(__file__), '..', 'pittsburgh', 'static')
 
 EXTRA_APPS = ()
